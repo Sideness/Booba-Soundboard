cd3828ca12714cf275e1b8a946415aa57935f63f
sideness <julien.stefani@y-nov.com>
2015-05-03 15:02:17 +0200

    Modification de deux fichiers de son Animation onclick

--- app/src/main/java/com/apps/sideness/boobasoundboard/MainActivity.java
+++ app/src/main/java/com/apps/sideness/boobasoundboard/MainActivity.java
@@ -1,10 +1,18 @@
 package com.apps.sideness.boobasoundboard;
 
+import android.app.Application;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v4.app.NotificationCompatSideChannelService;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
@@ -12,19 +20,48 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static Context context;
+    private static final float ROTATE_FROM = 0.0f;
+    private static final float ROTATE_TO = -3.0f * 360.0f;// 3.141592654f * 32.0f;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainActivity.context = getApplicationContext();
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
+        final GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(MainActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
+                                    final int position, long id) {
+                try{
+                    String pos = "";
+                    if(position == 0){
+                        pos = "im1";
+                    }else{
+                        pos = "im" + Integer.toString(position + 1);
+                    }
+                    final MediaPlayer mPlayer = MediaPlayer.create(context, getResources().getIdentifier(pos, "raw","com.apps.sideness.boobasoundboard"));
+                    mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mPlayer.start();
+
+                    RotateAnimation r = new RotateAnimation(ROTATE_FROM, ROTATE_TO, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                    r.setDuration((long) 2 * 1500);
+                    //gridview.getAdapter().getView(position, this, this);
+                    gridview.getChildAt(position - gridview.getFirstVisiblePosition()).startAnimation(r);
+                    mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                        public void onCompletion(MediaPlayer mp) {
+
+                            gridview.getChildAt(position - gridview.getFirstVisiblePosition()).clearAnimation();
+                        }
+                    });
+                }catch(Exception e){
+                    Log.e(e.getMessage().toString(), e.getMessage().toString());
+                }
+
             }
         });
     }
